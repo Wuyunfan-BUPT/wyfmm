f9feb28f11ff24e7c52a2328a07cf2e731f41467
Wuyunfan-BUPT <1564396096@qq.com>
2022-05-16 15:31:04 +0800

    dilation

---
+++ configs/_base_/models/upernet_SwinConvUnet_dilation.py
@@ -0,0 +1,65 @@
+# model settings
+norm_cfg = dict(type='BN', requires_grad=True)
+# norm_cfg = dict(type='BN', requires_grad=True)
+backbone_norm_cfg = dict(type='LN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    pretrained=None,
+    backbone=dict(
+        type='SwinConvUnet',
+        pretrain_img_size=224,
+        embed_dims=96,
+        patch_size=4,
+        window_size=7,
+        mlp_ratio=4,
+        depths=[2, 2, 6, 2],
+        num_heads=[3, 6, 12, 24],
+        strides=(4, 2, 2, 2),
+        out_indices=(0, 1, 2, 3),
+        qkv_bias=True,
+        qk_scale=None,
+        patch_norm=True,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.3,
+        use_abs_pos_embed=False,
+        act_cfg=dict(type='GELU'),
+        norm_cfg=backbone_norm_cfg,
+        dec_num_convs=(2, 2, 2, 2),
+        dec_dilations=(2, 3, 5, 7),
+        conv_cfg=None,
+        upsample_cfg=dict(type='InterpConv'),
+        conv_norm_cfg=dict(type='BN'),
+        conv_act_cfg=dict(type='ReLU')),
+    decode_head=dict(
+        type='UPerHead',
+        in_channels=[96, 192, 384, 768],
+        in_index=[0, 1, 2, 3],
+        pool_scales=(1, 2, 3, 6),
+        channels=512,
+        dropout_ratio=0.1,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        sampler=dict(type='OHEMPixelSampler', thresh=0.7, min_kept=100000),
+        loss_decode=[
+            dict(type='CrossEntropyLoss', use_sigmoid=False, loss_name='loss_ce', loss_weight=1.0),
+            dict(type='DiceLoss', loss_name='loss_dice', loss_weight=3.0)]),
+    auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=384,
+        in_index=2,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        sampler=dict(type='OHEMPixelSampler', thresh=0.7, min_kept=100000),
+        loss_decode=[
+            dict(type='CrossEntropyLoss', use_sigmoid=False, loss_name='loss_ce', loss_weight=0.4),
+            dict(type='DiceLoss', loss_name='loss_dice', loss_weight=3.0)]),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
