46e85799c4161560372ed3b44c264d584eab5efc
Wuyunfan-BUPT <1564396096@qq.com>
2022-05-25 09:52:30 +0800

    upper no index

---
+++ configs/_base_/models/upernet_Noindex_vanUnet.py
@@ -0,0 +1,39 @@
+# model settings
+#norm_cfg = dict(type='SyncBN', requires_grad=True)
+norm_cfg = dict(type='BN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    pretrained=None,
+    backbone=dict(
+        type='vanunet_tiny',
+        style='pytorch'),
+    decode_head=dict(
+        type='UPerHead',
+        in_channels=[32, 64, 160, 256],
+        in_index=[0, 1, 2, 3],
+        pool_scales=(1, 2, 3, 6),
+        channels=512,
+        dropout_ratio=0.1,
+        num_classes=150,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        #sampler=dict(type='OHEMPixelSampler', thresh=0.7, min_kept=100000),
+        ignore_index=0,
+        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0, avg_non_ignore=True)),
+    auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=160,
+        in_index=2,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=150,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        #sampler=dict(type='OHEMPixelSampler', thresh=0.7, min_kept=100000),
+        ignore_index=0,
+        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=False,  loss_weight=1.0, avg_non_ignore=True)),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
