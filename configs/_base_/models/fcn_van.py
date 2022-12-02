5fcb82bfbe6312dda96500461610ad5e461ccc61
Wuyunfan-BUPT <1564396096@qq.com>
2022-12-02 20:50:47 +0800

    change fcn_van model

---
+++ configs/_base_/models/fcn_van.py
@@ -0,0 +1,26 @@
+# model settings
+norm_cfg = dict(type='BN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    pretrained=None,
+    backbone=dict(
+        type='van_small',
+        style='pytorch'),
+    decode_head=dict(
+        type='FCNHead',
+        in_channels=512,
+        in_index=3,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=4,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        # sampler=dict(type='OHEMPixelSampler', thresh=0.7, min_kept=100000),
+        loss_decode=[
+            dict(type='CrossEntropyLoss', use_sigmoid=False, loss_name='loss_ce', loss_weight=1.0),
+            dict(type='DiceLoss', loss_name='loss_dice', loss_weight=3.0)]),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
