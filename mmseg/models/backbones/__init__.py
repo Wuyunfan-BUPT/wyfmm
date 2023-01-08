542ff5a3e575d749c6018623766cc1e26fbc388c
Wuyunfan-BUPT <1564396096@qq.com>
2023-01-08 12:00:31 +0800

    add bigpaper

--- mmseg/models/backbones/__init__.py
+++ mmseg/models/backbones/__init__.py
@@ -28,6 +28,7 @@ from .van import VAN
 from .swinL_convR_unet1 import SwinConvUnet1
 from .swinL_convR_unet2 import SwinConvUnet2
 from .swin_conv1 import SwinTransformer1
+from .swin_conv2 import SwinTransformer2
 __all__ = [
     'ResNet', 'ResNetV1c', 'ResNetV1d', 'ResNeXt', 'HRNet', 'FastSCNN',
     'ResNeSt', 'MobileNetV2', 'UNet', 'CGNet', 'MobileNetV3',
@@ -35,5 +36,5 @@ __all__ = [
     'BiSeNetV1', 'BiSeNetV2', 'ICNet', 'TIMMBackbone', 'ERFNet', 'PCPVT',
     'SVT', 'STDCNet', 'STDCContextPathNet', 'BEiT', 'MAE', 'SwinConvUnet',
     'convSwinUnet', 'VANUnet', 'UNet_re', 'VAN', 'SwinConvUnet1', 'SwinConvUnet2',
-    'SwinTransformer1'
+    'SwinTransformer1','SwinTransformer2'
 ]
