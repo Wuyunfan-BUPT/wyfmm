b19c4432a1acbfcb6176b04cdcc1853f67087b98
Wuyunfan-BUPT <1564396096@qq.com>
2023-02-16 22:19:10 +0800

    add bigpaper1

--- configs/final/kaggle/deeplav_res_zcross.py
+++ configs/final/kaggle/deeplav_res_zcross.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../../_base_/models/deeplabv3_r50_zcross.py', '../../_base_/datasets/brats2020_zcross_kaggle.py',
+    '../../_base_/models/deeplabv3_r50_zcross.py', '../../_base_/datasets/brats2020_zcrossP_kaggle.py',
     '../../_base_/default_runtime.py', '../../_base_/schedules/schedule_160k_dice.py'
 ]
 
